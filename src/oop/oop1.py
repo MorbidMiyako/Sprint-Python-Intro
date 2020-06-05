ef07cd78f8f395a1ca88eff45a3b5d10eb675738
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-06-05 17:13:43 +0200

    finished OOP 1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,35 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+"""
+Lowest class is Vehicle
+"""
+
+
+class Vehicle:
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
